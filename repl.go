5921da1424b6fa30cb144a0a9a0723859576923e
SGWinter <seangcalgary@gmail.com>
2025-07-01 11:42:21 -0600

    cleaning input and command started

--- repl.go
+++ repl.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
+
+func startREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		inputText := cleanInput(scanner.Text())
+		if len(inputText) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %v\n", inputText[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
