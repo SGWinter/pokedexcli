2ba4962172aff4513ea31fcd0b3884d37d96b915
SGWinter <seangcalgary@gmail.com>
2025-07-10 16:02:16 -0600

    cache solved

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,19 +3,23 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/SGWinter/pokedexcli/internal/pokecache"
 )
 
 // Client -
 type Client struct {
 	httpClient http.Client
+	cache      pokecache.Cache
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, interval time.Duration) Client {
 	return Client{
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
+		cache:     pokecache.NewCache(interval),
 	}
 }
 
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,61 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mu    *sync.Mutex
+}
+
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache (interval time.Duration) Cache {
+	nCache := Cache{
+		cache: make(map[string]cacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	go nCache.reapLoop(interval)
+
+	return nCache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.cache[key]
+	return entry.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
+}
